1f0adaf1172aa33114aee613d056a22def94fdcb
yoomster <nperenboom@hotmail.com>
2025-04-20 15:28:46 +0200

    practise methods

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
   data() {
     return {
+      showBooks: true,
       title: "The Final Empire",
       author: "Brandon Sanderson",
       age: 50,
@@ -9,6 +10,9 @@ const app = Vue.createApp({
   methods: {
     changeTitle(title) {
       this.title = title;
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
