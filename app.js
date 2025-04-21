87abb2c76e890101291abb8667fca8c971ee4add
yoomster <nperenboom@hotmail.com>
2025-04-22 00:47:34 +0200

    latest

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ const app = Vue.createApp({
           title: "The Final Empire",
           author: "Brandon Sanderson",
           img: "assets/The Final Empire.jpg",
+          isFav: true,
         },
         {
           title: "The Way Of Kings",
           author: "Brandon Sanderson",
           img: "assets/The Way Of Kings.jpg",
+          isFav: false,
         },
         {
           title: "Name Of The Wind",
           author: "Patrick Rothfuss",
           img: "assets/Name Of The Wind.jpg",
+          isFav: true,
         },
       ],
     };
